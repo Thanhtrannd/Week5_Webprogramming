13f28202c01831b4147d6f998e56c2bb2ff47221
Thanh Tran <tndt.1902@gmail.com>
2022-10-05 19:14:23 +0300

    Implement map and GeoJson data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+const fetchData = async () => {
+  const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
+  const res = await fetch(url)
+  const data = await res.json()
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  initMap(data)
+};
+
+const initMap = (data) => {
+  let map = L.map('map', {
+    minZoom: -3
+  })
+
+  let geoJson = L.geoJson(data, {
+    weight: 2,
+    onEachFeature: getFeature
+  }).addTo(map)
+
+  let osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  map.fitBounds(geoJson.getBounds())
+
+}
+
+const getFeature = (feature, layer) => {
+
+}
+
+fetchData()
